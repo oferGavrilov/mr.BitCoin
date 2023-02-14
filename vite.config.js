0d7fa4caa87ef6ee48fea6be37bc494378f68152
ofer <ofergavri@gmail.com>
2023-02-14 20:52:09 +0200

    add build

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/mr.BitCoin/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
@@ -12,3 +13,6 @@ export default defineConfig({
     }
   }
 })
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/mr.BitCoin/" : "/",
+// };
