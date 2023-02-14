b13c944cbcd24982228e83123a30e1ada5f608d4
ofer <ofergavri@gmail.com>
2023-02-14 21:04:02 +0200

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/mr.BitCoin/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
   }
 })
 // module.exports = {
 //   publicPath: process.env.NODE_ENV === "production" ? "/mr.BitCoin/" : "/",
-// };
+// },
